555ba29feb678adf3646d5e8c5d46699b874c55b
Subtlecobra3864 <adamcarrington.ac@gmail.com>
2024-04-06 08:04:30 +0100

    aduoghodughb

---
+++ nvim/lua/adam/options.lua
@@ -0,0 +1,27 @@
+vim.opt.guicursor = "n-v-c-i:block"
+
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.termguicolors = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.shortmess:append({c = true})
+
+vim.opt.updatetime = 100
+vim.opt.colorcolumn = "80"
